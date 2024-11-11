74689b23e1e9b4ae9e9949fa9425dab323239487
Jarvasio <149535389+Jarvasio@users.noreply.github.com>
2024-11-11 11:06:43 +0000

    Add files via upload

---
+++ projenerg.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+# Título do aplicativo
+st.set_page_config(page_title="Projeto de energia CGASC", page_icon= "bar_chart:", layout= "wide")
+
+st.title("Projeto de energia CGASC")
