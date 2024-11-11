c7b20b685468421082ca83175c3fa5f144c24ca1
Jarvasio <149535389+Jarvasio@users.noreply.github.com>
2024-11-11 16:39:48 +0000

    Update projenerg.py

--- projenerg.py
+++ projenerg.py
@@ -1,9 +1,38 @@
-import streamlit as st
-import os
-import warnings
-warnings.filterwarnings('ignore')
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
 
-# Título do aplicativo
-st.set_page_config(page_title="Projeto de energia CGASC", page_icon= "bar_chart:", layout= "wide")
+# Inicialize o aplicativo Dash
+app = dash.Dash(__name__)
 
-st.title("Projeto de energia CGASC")
+# Carregue seus dados
+df = pd.read_csv('seu_arquivo_de_dados.csv')
+
+# Defina o layout do aplicativo
+app.layout = html.Div([
+    html.H1('Meu Dashboard'),
+    
+    dcc.Dropdown(
+        id='dropdown-seletor',
+        options=[{'label': i, 'value': i} for i in df['coluna'].unique()],
+        value=df['coluna'].iloc[0]
+    ),
+    
+    dcc.Graph(id='grafico-principal')
+])
+
+# Defina callbacks para interatividade
+@app.callback(
+    Output('grafico-principal', 'figure'),
+    Input('dropdown-seletor', 'value')
+)
+def atualizar_grafico(valor_selecionado):
+    dados_filtrados = df[df['coluna'] == valor_selecionado]
+    fig = px.line(dados_filtrados, x='data', y='valor')
+    return fig
+
+# Execute o aplicativo
+if __name__ == '__main__':
+    app.run_server(debug=True)
